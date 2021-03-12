570d779f6209f07c536f30990845ea39209cabfe
AndrewAntepa <55337950+AndrewAntepa@users.noreply.github.com>
2021-03-12 23:33:45 +0800

    Initial commit

---
+++ app/src/main/java/com/example/hash/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.hash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    TextView hash, tree;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.text);
+        hash = findViewById(R.id.textHash);
+        tree = findViewById(R.id.textTree);
+        button = findViewById(R.id.button);
+        TreeSet<String> treeSet = new TreeSet<>();
+        HashSet<String> hashSet = new HashSet<>();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hashSet.add(editText.getText().toString());
+                Iterator<String> iteratorH = hashSet.iterator();
+                hash.setText("");
+                while (iteratorH.hasNext()) {
+                    hash.append(iteratorH.next() + "\n");
+                }
+
+                treeSet.add(editText.getText().toString());
+                Iterator<String> iteratorT = treeSet.iterator();
+                tree.setText("");
+                while (iteratorT.hasNext()) {
+                    tree.append(iteratorT + "\n");
+                }
+
+                editText.setText("");
+            }
+        });
+    }
+}
